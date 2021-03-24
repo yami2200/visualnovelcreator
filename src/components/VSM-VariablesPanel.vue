454eb86f32a5f91737c82b0adfac2331ee88af78
yami2200 <TheYami22@gmail.com>
2021-03-24 21:17:53 +0100

    Starting variables panel

---
+++ src/components/VSM-VariablesPanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="500px"
+  >
+    <v-card>
+      <v-card-title>
+        <span class="headline" v-text="'Global Variables :'"></span>
+      </v-card-title>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "VSM-VariablesPanel",
+
+  data () {
+    return {
+      dialog: false,
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
