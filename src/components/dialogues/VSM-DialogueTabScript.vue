cd255dd7c60aa2c28527110e7d3efd17a4f97b09
yami2200 <TheYami22@gmail.com>
2021-06-06 18:00:49 +0200

    Adding feature to prevent the player to go through others dialogue before all the scripts were executed.

--- src/components/dialogues/VSM-DialogueTabScript.vue
+++ src/components/dialogues/VSM-DialogueTabScript.vue
@@ -1,19 +1,21 @@
 <template>
   <v-card flat height="500">
-    <v-row class="mt-1">
+    <v-checkbox
+        class="ml-2"
+        v-model="current.waitEndScript"
+        label="Wait the end of all script before leaving the dialogue:"
+    ></v-checkbox>
       <v-card
           class="mx-auto"
           width="100%"
-          height="480"
+          height="450"
           tile
       >
-        <v-list class="mt-2 overflow-y-auto" max-height="480" dense>
+        <v-list class="mt-2 overflow-y-auto" max-height="450" dense>
           <vsm-scriptlist :assets="assets" :action="current.action" :indentation="0"> </vsm-scriptlist>
         </v-list>
 
       </v-card>
-
-    </v-row>
   </v-card>
 </template>
 
